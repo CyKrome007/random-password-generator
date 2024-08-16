b0ae21ea8f2d08ff6e318a1d4b419805c12dfdcd
CyKrome007 <OwaizKhan1111@gmail.com>
2024-08-16 15:22:52 +0530

    Modified UI

--- src/PasswordGenerator.jsx
+++ src/PasswordGenerator.jsx
@@ -61,56 +61,60 @@ const PasswordGenerator = () => {
     return(
         <>
             <div
-                className="flex flex-row min-h-screen items-center justify-center mx-auto duration-200"
+                className="flex flex-col min-h-screen items-center justify-center mx-auto duration-200"
                 style={{
                     background: color,
                     color: textColor,
                     borderColor: border_color
                 }}>
+                <button className={'px-4 py-2 text-3xl border-2 rounded-full absolute right-5 top-5'}
+                        onClick={() => {
+                            setColor(color === 'white' ? 'black' : 'white');
+                            setTextColor((color === 'white') ? 'white' : 'black');
+                            setShadowColor((color === 'white') ? 'black' : 'white');
+                        }
+                }>
+                    <FontAwesomeIcon icon={color === "white" ? faMoon : faSun}/>
+                </button>
                 <div>
-
                     <div
-                        className={'flex rounded-l-full rounded-r-2xl border-2 shadow-xl'}
+                        className={'flex rounded-l-2xl rounded-r-2xl border-2 shadow-xl'}
                         style={{
                             borderColor: border_color,
                             boxShadow: shadowColor,
                         }}
                     >
-                        <button className={'px-10 text-5xl'} onClick={() => {
-                            setColor(color === 'white' ? 'black' : 'white');
-                            setTextColor((color === 'white') ? 'white' : 'black');
-                            setShadowColor((color === 'white') ? 'black' : 'white');
-                        }}>
-                            <FontAwesomeIcon icon={color === "white" ? faMoon : faSun}/>
-                        </button>
-                        <div className={'flex flex-col border-l-2 p-2'} style={{borderColor: border_color}}>
 
-                            <div className={'flex flex-row justify-center items-center border-b-2 p-2 px-10'}
+                        <div className={'flex flex-col p-2'} style={{borderColor: border_color}}>
+
+                            <div className={'flex flex-col justify-center items-center border-b-2 p-2 px-10'}
                                  style={{borderColor: border_color}}>
-                                <div className={'mx-2'}>Password Generator</div>
-                                <input
-                                    type={'text'}
-                                    className={'px-3 py-2 rounded-l-full border-l-2 border-y-2'}
-                                    style={{
-                                        borderColor: border_color,
-                                        color: "white",
-                                    }}
-                                    placeholder={'Password'}
-                                    value={password}
-                                    ref={passwordRef}
-                                    readOnly
-                                />
-                                <button
-                                    className={'px-3 py-2 border-r-2 border-y-2 rounded-r-full'}
-                                    style={{
-                                        borderColor: border_color,
-                                    }}
-                                    ref={copyButtonRef}
-                                    onClick={copyPasswordToClipboard}
+                                <div className={'mb-3 text-3xl font-bold'}>Password Generator</div>
+                                <div>
+                                    <input
+                                        type={'text'}
+                                        className={'px-3 py-2 rounded-l-full border-l-2 border-y-2'}
+                                        style={{
+                                            borderColor: border_color,
+                                            color: "white",
+                                        }}
+                                        placeholder={'Password'}
+                                        value={password}
+                                        ref={passwordRef}
+                                        readOnly
+                                    />
+                                    <button
+                                        className={'px-3 py-2 border-r-2 border-y-2 rounded-r-full'}
+                                        style={{
+                                            borderColor: border_color,
+                                        }}
+                                        ref={copyButtonRef}
+                                        onClick={copyPasswordToClipboard}
 
-                                >
-                                    <FontAwesomeIcon icon={faCopy}/>
-                                </button>
+                                    >
+                                        <FontAwesomeIcon icon={faCopy}/>
+                                    </button>
+                                </div>
                             </div>
                             <div className={'px-16 justify-center items-center'}
                                  style={{borderColor: border_color}}>
@@ -126,28 +130,43 @@ const PasswordGenerator = () => {
                                     }}
                                 />
                             </div>
-                            <div className={'flex flex-row flex-nowrap justify-center items-center pb-2 gap-4'}
+                            <div className={'flex flex-col flex-nowrap justify-center items-center pb-2 gap-4'}
                                  style={{borderColor: border_color}}>
 
-                                <p>Length -&gt; {length}</p>
+                                <p className={'text-2xl'}>Length -&gt; {length}</p>
 
-                                <input type={'checkbox'} defaultChecked={numberAllowed}
-                                       id={'numberInput'}
-                                       className={"w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"}
-                                       onChange={() => setNumberAllowed((prev) => !prev)}
-                                />
-                                <label htmlFor={'numberInput'}>Number</label>
+                                <div
+                                    className={'flex flex-row flex-nowrap justify-center items-center px-2 pt-4 gap-x-4 border-t-2'}
+                                    style={{
+                                        borderColor: border_color,
+                                    }}
+                                >
+                                    <input type={'checkbox'} defaultChecked={numberAllowed}
+                                           id={'numberInput'}
+                                           className={"w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"}
+                                           onChange={() => setNumberAllowed((prev) => !prev)}
+                                    />
+                                    <label htmlFor={'numberInput'}>Number</label>
 
-                                <input type={'checkbox'} defaultChecked={specialCharactersAllowed}
-                                       id={'specialCharacterInput'}
-                                       className={"w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"}
-                                       onChange={() => setSpecialCharactersAllowed(prev => !prev)}
-                                />
-                                <label htmlFor={'specialCharacterInput'}>Special Character</label>
+                                    <input type={'checkbox'} defaultChecked={specialCharactersAllowed}
+                                           id={'specialCharacterInput'}
+                                           className={"w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"}
+                                           onChange={() => setSpecialCharactersAllowed(prev => !prev)}
+                                    />
+                                    <label htmlFor={'specialCharacterInput'}>Special Character</label>
 
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div
+                    className={'flex absolute bottom-0 w-full items-center justify-center bg-black'}
+                    style={{
+                        color: textColor === "white" ? "#0F0" : "white",
+                    }}
+                >
+                    Made By CyKrome
                 </div>
             </div>
         </>
